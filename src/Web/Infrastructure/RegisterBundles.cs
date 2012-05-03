56bac23ccfb65689afb235d788ef1821dd4a6e7c
hazzik <hazzik@gmail.com>
2012-05-03 20:55:28 +0600

    WTF?

--- src/Web/Infrastructure/RegisterBundles.cs
+++ src/Web/Infrastructure/RegisterBundles.cs
@@ -18,27 +18,27 @@
             var nullOrderer = new NullOrderer();
 
             var commonStylesBundle = new Bundle("~/CommonStyles", cssTransformer);
-            commonStylesBundle.AddFile("~/Content/site.less");
-            commonStylesBundle.AddFile("~/Content/themes/base/jquery-ui.css");
-            commonStylesBundle.AddFile("~/Content/bootstrap.min.css");
-            commonStylesBundle.AddFile("~/Content/bootstrap-responsive.min.css");
+            commonStylesBundle.AddFile("~/Content/site.less", false);
+            commonStylesBundle.AddFile("~/Content/themes/base/jquery-ui.css", false);
+            commonStylesBundle.AddFile("~/Content/bootstrap.min.css", false);
+            commonStylesBundle.AddFile("~/Content/bootstrap-responsive.min.css", false);
             commonStylesBundle.Orderer = nullOrderer;
 
             BundleTable.Bundles.Add(commonStylesBundle);
 
             var modernizrBundle = new Bundle("~/Modernizr", jsTransformer);
-            modernizrBundle.AddFile("~/Scripts/modernizr-2.5.3.js");
+            modernizrBundle.AddFile("~/Scripts/modernizr-2.5.3.js", false);
             modernizrBundle.Orderer = nullOrderer;
 
             BundleTable.Bundles.Add(modernizrBundle);
 
             var commonScriptsBundle = new Bundle("~/CommonScripts", jsTransformer);
-            commonScriptsBundle.AddFile("~/Scripts/jquery-1.7.2.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery-ui-1.8.19.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.unobtrusive.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.unobtrusive-ajax.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/bootstrap.min.js");
+            commonScriptsBundle.AddFile("~/Scripts/jquery-1.7.2.min.js", false);
+            commonScriptsBundle.AddFile("~/Scripts/jquery-ui-1.8.19.min.js", false);
+            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.min.js", false);
+            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.unobtrusive.min.js", false);
+            commonScriptsBundle.AddFile("~/Scripts/jquery.unobtrusive-ajax.min.js", false);
+            commonScriptsBundle.AddFile("~/Scripts/bootstrap.min.js", false);
             commonScriptsBundle.Orderer = nullOrderer;
 
             BundleTable.Bundles.Add(commonScriptsBundle);
